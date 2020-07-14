44d879146dc07da413065febee59c8b3df02e89c
Rob Bishop <c.sagan.ritual@gmail.com>
2020-07-13 18:47:53 -0700

    Initial Commit

---
+++ Fletch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Fletch
+//
+//  Created by Rob Bishop on 7/13/20.
+//  Copyright © 2020 Boring Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
